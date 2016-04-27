c324fb077a83180e73a8ee590a4d93a66903737c
Daniel Howe <daniel@rednoise.org>
2016-04-27 12:00:12 +0800

    Added example 5

---
+++ Example5/content.js
@@ -0,0 +1,11 @@
+// content.js
+
+// Create the text for the CSS we need for our font
+var fontFace = '@font-face { font-family: Redacted; src: url("' +
+  chrome.extension.getURL('fonts/redacted-regular.woff') + '"); }';
+
+// Create a style tag for our CSS and inject it into the page
+$("<style>").prop("type", "text/css").html(fontFace).appendTo("head");
+
+// Apply our font-family to *every* element (in black)
+$("*").css('font-family', 'Redacted').css('color', '#000');
